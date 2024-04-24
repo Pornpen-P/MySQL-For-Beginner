c8e13a78ed51da7c66c52b7147d7366942e57b5d
Pornpen -P <pornpen@welovebug.biz>
2024-04-24 17:16:54 +0700

    [Updated]: เพิ่ม Code Insertdata

---
+++ Insertdata.sql
@@ -0,0 +1,9 @@
+-- เพิ่มสินค้าลงในฐานข้อมูล 1 รายการ
+insert into products (Product_Name,Product_Price,Product_Unit)
+values ('HACIPUPU The Sweetheart Figure',1050.00,'PCS');
+
+
+-- เพิ่มสินค้าลงในฐานข้อมูลหลายรายการ
+insert into products (Product_Name,Product_Price,Product_Unit)
+values ('CRYBABY × Powerpuff Girls Series-Vinyl Face Plush Blind Box',430.00,'PCS'),('Molly Beach Music Festival Figure',2390.00,'PCS'),('THE MONSTERS FALL IN WILD SERIES-Vinyl Plush Doll Pendant',850,'PCS');
+
