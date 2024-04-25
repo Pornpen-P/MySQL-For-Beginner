2b926090e405b56d02e1ea3822dc19668c93522f
Pornpen -P <pornpen@welovebug.biz>
2024-04-25 18:14:39 +0700

    [Created]: เพิ่มมูลในตาราง Product_notes

--- OnetooneCreate.sql
+++ OnetooneCreate.sql
@@ -1,6 +1,6 @@
--- สร้างตารางแบบเชื่อมต่อกับตารางเดิม โดยกำหนดค่า FK 
+-- สร้างตารางแบบเชื่อมต่อกับตารางเดิม โดยกำหนดค่า FK
 CREATE TABLE `SCK_Shopingmall`.`Product_notes` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `note` TEXT(200) NOT NULL,
   `Product_id` INT NOT NULL,
   PRIMARY KEY (`id`),
@@ -10,4 +10,9 @@ CREATE TABLE `SCK_Shopingmall`.`Product_notes` (
     REFERENCES `SCK_Shopingmall`.`Products` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE);
+
+
+-- เพิ่มข้อมูลในตารางเชื่อมต่อ
+INSERT INTO `SCK_Shopingmall`.`Product_notes` (`note`, `Product_id`) VALUES ('น่ารักมาก', '1');
+
 
