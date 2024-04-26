383ce3f2c17e9cb82e37003c2aa3121a5a5e91d9
Pornpen -P <pornpen@welovebug.biz>
2024-04-26 18:26:21 +0700

    [Edited]: แก้ไขคอมเมนต์

--- OnetomanyCreate.sql
+++ OnetomanyCreate.sql
@@ -1,5 +1,5 @@
 
-สร้างตาราง
+-- สร้างตาราง Categories
 CREATE TABLE `SCK_Shopingmall`.`Categories` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `Category_Name` VARCHAR(200) NOT NULL,
@@ -7,7 +7,8 @@ CREATE TABLE `SCK_Shopingmall`.`Categories` (
   UNIQUE INDEX `Category_Name_UNIQUE` (`Category_Name` ASC) VISIBLE);
 
 
-แก้ไขโครงสร้างตาราง
+
+-- แก้ไขโครงสร้างตาราง Products โดยการเพิ่มฟิลด์ Category_id
 ALTER TABLE `SCK_Shopingmall`.`Products`
 ADD COLUMN `Category_id` INT NULL AFTER `Product_Created`,
 ADD INDEX `fk_products_categories_idx` (`Category_id` ASC) VISIBLE;
@@ -19,11 +20,11 @@ ADD CONSTRAINT `fk_products_categories`
   ON DELETE CASCADE
   ON UPDATE CASCADE;
 
-เพิ่มข้อมูล
+-- เพิ่มข้อมูล Category_Name
 INSERT INTO `SCK_Shopingmall`.`Categories` (`Category_Name`) VALUES ('กล่องสุ่ม');
 INSERT INTO `SCK_Shopingmall`.`Categories` (`Category_Name`) VALUES ('ตุ๊กตาตัวใหญ่');
 
-อัปเดตข้อมูล
+-- อัปเดตข้อมูล Category_id
 UPDATE `SCK_Shopingmall`.`Products` SET `Category_id` = '1' WHERE (`id` = '1');
 UPDATE `SCK_Shopingmall`.`Products` SET `Category_id` = '1' WHERE (`id` = '2');
 UPDATE `SCK_Shopingmall`.`Products` SET `Category_id` = '1' WHERE (`id` = '3');
@@ -33,6 +34,7 @@ UPDATE `SCK_Shopingmall`.`Products` SET `Category_id` = '2' WHERE (`id` = '6');
 UPDATE `SCK_Shopingmall`.`Products` SET `Category_id` = '2' WHERE (`id` = '7');
 
 
+-- แก้ไขโครงสร้างตาราง Product_notes โดยการกำหนดให้ Product_id มีความ Unique
 ALTER TABLE `SCK_Shopingmall`.`Product_notes`
 ;
 ALTER TABLE `SCK_Shopingmall`.`Product_notes` ALTER INDEX `Product_id_UNIQUE` VISIBLE;
